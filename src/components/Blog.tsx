822f9e072877694cbbe4a01a71b540dbf4e66da5
Leyban <96819910+Leyban@users.noreply.github.com>
2023-02-07 17:18:47 +0800

    fixed alt error

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -8,7 +8,7 @@ export interface BlogProps {
 export const Blog:React.FC<BlogProps> = (props) => {
     return(
         <div className="blog-container">
-            <img src={props.image} alt="blog image" />
+            <img src={props.image} alt="blog" />
             <p className="author">By {props.author}</p>
             <h3>{props.title}</h3>
             <p>{props.content}</p>
